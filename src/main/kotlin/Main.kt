edef880f082482a995f6d9be47e9f5ec43860ac7
Jo Hanna Pearce <jo@nrwl.io>
2022-11-24 15:04:49 +0000

    very basic attempt to login with IAM role temp credentials

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,16 @@
-import com.amazonaws.HttpMethod
+import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
-import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.auth.BasicSessionCredentials
+import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.S3ClientOptions
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
+import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
+import com.amazonaws.services.securitytoken.model.AssumeRoleRequest
+import com.amazonaws.services.securitytoken.model.AssumeRoleResult
+import com.amazonaws.services.securitytoken.model.Credentials
 import org.slf4j.LoggerFactory.getLogger
-import kotlinx.coroutines.runBlocking
 import java.util.*
 
 private val log = getLogger("AwsTest")
@@ -28,21 +34,58 @@ const val KEY = "key"
  * AWS_ACCESS_KEY_ID
  * AWS_SECRET_ACCESS_KEY
  *
- * So we can switch on
+ * So we can switch on whether we have AWS_ACCESS_KEY_ID or AWS_ROLE_ARN
  */
 fun main() {
-    val s3AccessKey = System.getenv("AWS_ACCESS_KEY_ID")
-    val s3SecretKey = System.getenv("AWS_SECRET_ACCESS_KEY")
-    val s3Endpoint = System.getenv("AWS_S3_ENDPOINT")
-    val s3Accelerated = System.getenv("AWS_S3_ACCELERATED") == "TRUE"
+    val env = System.getenv()
 
-    log.info("AWS_ACCESS_KEY_ID = $s3AccessKey")
+    val s3: AmazonS3 = if (env.containsKey("AWS_ROLE_ARN")) {
+        log.info("Role ARN provided, attempting to create client with temp credentials.")
 
-    val s3 = AmazonS3Client(BasicAWSCredentials(s3AccessKey, s3SecretKey))
-    if (s3Endpoint != null) {
-        s3.setEndpoint(s3Endpoint)
+        val stsClient = AWSSecurityTokenServiceClientBuilder.standard()
+            .withCredentials(ProfileCredentialsProvider())
+            .withRegion(Regions.DEFAULT_REGION)
+            .build()
+
+        val roleRequest = AssumeRoleRequest()
+            .withRoleArn(env["AWS_ROLE_ARN"])
+            .withRoleSessionName("nxApiRoleSession")
+
+        val roleResponse: AssumeRoleResult = stsClient.assumeRole(roleRequest)
+        val sessionCredentials: Credentials = roleResponse.credentials
+
+        AmazonS3ClientBuilder.standard()
+            .withCredentials(
+                AWSStaticCredentialsProvider(
+                    BasicSessionCredentials(
+                        sessionCredentials.accessKeyId,
+                        sessionCredentials.secretAccessKey,
+                        sessionCredentials.sessionToken
+                    )
+                )
+            )
+            .withRegion(Regions.DEFAULT_REGION)
+            .build()
+    } else if (env.containsKey("AWS_ACCESS_KEY_ID") && env.containsKey("AWS_SECRET_ACCESS_KEY")) {
+        log.info("AWS_ACCESS_KEY_ID provided, creating client the old-fashioned way.")
+        AmazonS3ClientBuilder.standard()
+            .withCredentials(
+                AWSStaticCredentialsProvider(
+                    BasicAWSCredentials(env["AWS_ACCESS_KEY_ID"], env["AWS_SECRET_ACCESS_KEY"])
+                )
+            )
+            .withRegion(Regions.DEFAULT_REGION)
+            .build()
+    } else {
+        val message = "No S3 credentials provided"
+        log.error(message)
+        throw Exception(message)
     }
-    if (s3Accelerated) {
+
+    if (env.containsKey("AWS_S3_ENDPOINT")) {
+        s3.setEndpoint(env["AWS_S3_ENDPOINT"])
+    }
+    if (env.containsKey("AWS_S3_ACCELERATED") && env["AWS_S3_ACCELERATED"] == "TRUE") {
         s3.setS3ClientOptions(S3ClientOptions.builder().setAccelerateModeEnabled(true).build())
     }
 
@@ -55,15 +98,15 @@ fun main() {
 }
 
 
-fun setupTutorial(s3: AmazonS3Client) {
+fun setupTutorial(s3: AmazonS3) {
     log.info("Creating bucket $BUCKET...")
     s3.createBucket(BUCKET, REGION)
     log.info("Bucket $BUCKET created successfully!")
 }
 
-fun cleanUp(s3: AmazonS3Client) {
+fun cleanUp(s3: AmazonS3) {
     log.info("Deleting object $BUCKET/$KEY...")
-    s3.deleteObject (BUCKET, KEY)
+    s3.deleteObject(BUCKET, KEY)
     log.info("Object $BUCKET/$KEY deleted successfully!")
 
     log.info("Deleting bucket $BUCKET...")
