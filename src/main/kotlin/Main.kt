fa265814f208ff4d17b4c57abd82145da70bcf01
Jo Hanna Pearce <jo@nrwl.io>
2022-11-29 17:08:05 +0000

    feat: add check for authentication

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,28 +2,24 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.auth.BasicSessionCredentials
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.S3ClientOptions
+import com.amazonaws.services.s3.model.ListObjectsRequest
 import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClientBuilder
-import com.amazonaws.services.securitytoken.model.AssumeRoleRequest
-import com.amazonaws.services.securitytoken.model.AssumeRoleResult
 import com.amazonaws.services.securitytoken.model.AssumeRoleWithWebIdentityRequest
 import com.amazonaws.services.securitytoken.model.AssumeRoleWithWebIdentityResult
 import com.amazonaws.services.securitytoken.model.Credentials
 import org.slf4j.LoggerFactory.getLogger
 import java.io.File
 import java.util.*
-import java.util.logging.Handler
 
 private val log = getLogger("AwsTest")
 
 const val REGION = "us-east-1"
 val BUCKET = "nx-cache-dev"
 //val BUCKET = "bucket-${UUID.randomUUID()}"
-const val KEY = "key"
 
 /**
  * When starting up the pod, if configured correctly for IAM Role access
@@ -44,8 +40,50 @@ const val KEY = "key"
  */
 fun main() {
     val env = System.getenv()
+    var s3: AmazonS3 = getClient(env)
 
-    val s3: AmazonS3 = if (env.containsKey("AWS_ROLE_ARN")) {
+    if (env.containsKey("AWS_S3_ENDPOINT")) {
+        s3.setEndpoint(env["AWS_S3_ENDPOINT"])
+    }
+    if (env.containsKey("AWS_S3_ACCELERATED") && env["AWS_S3_ACCELERATED"] == "TRUE") {
+        s3.setS3ClientOptions(S3ClientOptions.builder().setAccelerateModeEnabled(true).build())
+    }
+
+    log.info("Starting timer with delay....")
+
+    val timer = Timer()
+    timer.schedule(object : TimerTask() {
+        override fun run() {
+            log.info("Checking whether we are still authenticated...")
+            if (!isStillAuthenticated(s3)) {
+                log.info("Getting a new client.")
+                s3.shutdown()
+                s3 = getClient(env)
+            }
+
+            log.info("Executing with delay...")
+            val key = UUID.randomUUID().toString()
+            s3.putObject(BUCKET, key, "Testing with Kotlin SDK")
+            log.info("Object $BUCKET/$key created successfully!")
+            cleanUp(s3, key)
+        }
+    }, 0, 60000)
+
+
+}
+
+fun isStillAuthenticated(s3: AmazonS3): Boolean {
+    return try {
+        s3.listObjects(ListObjectsRequest(BUCKET, null, null, null,0))
+        true
+    } catch (ex: Exception) {
+        log.info("No longer authenticated with the current credentials.")
+        false
+    }
+}
+
+fun getClient(env: Map<String, String>): AmazonS3 {
+    return if (env.containsKey("AWS_ROLE_ARN")) {
         log.info("Role ARN provided, attempting to create client with temp credentials.")
 
         val stsClient = AWSSecurityTokenServiceClientBuilder.standard()
@@ -89,44 +127,10 @@ fun main() {
         log.error(message)
         throw Exception(message)
     }
-
-    if (env.containsKey("AWS_S3_ENDPOINT")) {
-        s3.setEndpoint(env["AWS_S3_ENDPOINT"])
-    }
-    if (env.containsKey("AWS_S3_ACCELERATED") && env["AWS_S3_ACCELERATED"] == "TRUE") {
-        s3.setS3ClientOptions(S3ClientOptions.builder().setAccelerateModeEnabled(true).build())
-    }
-
-//    setupTutorial(s3)
-
-    log.info("Starting timer with delay....")
-
-    val timer = Timer()
-    timer.schedule(object : TimerTask() {
-        override fun run() {
-            println("Executing with delay...")
-            s3.putObject(BUCKET, KEY, "Testing with Kotlin SDK")
-            log.info("Object $BUCKET/$KEY created successfully!")
-            cleanUp(s3)
-        }
-    }, 0, 60000)
-
-
 }
 
-
-fun setupTutorial(s3: AmazonS3) {
-    log.info("Creating bucket $BUCKET...")
-    s3.createBucket(BUCKET, REGION)
-    log.info("Bucket $BUCKET created successfully!")
-}
-
-fun cleanUp(s3: AmazonS3) {
-    log.info("Deleting object $BUCKET/$KEY...")
-    s3.deleteObject(BUCKET, KEY)
-    log.info("Object $BUCKET/$KEY deleted successfully!")
-
-//    log.info("Deleting bucket $BUCKET...")
-//    s3.deleteBucket(BUCKET)
-//    log.info("Bucket $BUCKET deleted successfully!")
+fun cleanUp(s3: AmazonS3, key: String) {
+    log.info("Deleting object $BUCKET/$key...")
+    s3.deleteObject(BUCKET, key)
+    log.info("Object $BUCKET/$key deleted successfully!")
 }
