5b2f4411f5d0850494be170d370860d7e52dbb0c
Jo Hanna Pearce <jo@nrwl.io>
2022-12-01 13:22:08 +0000

    update with file server class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.auth.BasicSessionCredentials
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.S3ClientOptions
@@ -17,8 +16,7 @@ import java.util.*
 
 private val log = getLogger("AwsTest")
 
-const val REGION = "us-east-1"
-val BUCKET = "nx-cache-dev"
+const val BUCKET = "nx-cache-dev"
 //val BUCKET = "bucket-${UUID.randomUUID()}"
 
 /**
@@ -40,97 +38,139 @@ val BUCKET = "nx-cache-dev"
  */
 fun main() {
     val env = System.getenv()
-    var s3: AmazonS3 = getClient(env)
-
-    if (env.containsKey("AWS_S3_ENDPOINT")) {
-        s3.setEndpoint(env["AWS_S3_ENDPOINT"])
-    }
-    if (env.containsKey("AWS_S3_ACCELERATED") && env["AWS_S3_ACCELERATED"] == "TRUE") {
-        s3.setS3ClientOptions(S3ClientOptions.builder().setAccelerateModeEnabled(true).build())
-    }
+    val fileServer = S3BasedFileServer(env)
 
     log.info("Starting timer with delay....")
 
     val timer = Timer()
     timer.schedule(object : TimerTask() {
         override fun run() {
-            log.info("Checking whether we are still authenticated...")
-            if (!isStillAuthenticated(s3)) {
-                log.info("Getting a new client.")
-                s3.shutdown()
-                s3 = getClient(env)
-            }
-
             log.info("Executing with delay...")
             val key = UUID.randomUUID().toString()
-            s3.putObject(BUCKET, key, "Testing with Kotlin SDK")
-            log.info("Object $BUCKET/$key created successfully!")
-            cleanUp(s3, key)
+            fileServer.putObject(key)
+            fileServer.cleanUp(key)
         }
     }, 0, 60000)
 
 
 }
 
-fun isStillAuthenticated(s3: AmazonS3): Boolean {
-    return try {
-        s3.listObjects(ListObjectsRequest(BUCKET, null, null, null,0))
-        true
-    } catch (ex: Exception) {
-        log.info("No longer authenticated with the current credentials.")
-        false
+class S3BasedFileServer(private val env: Map<String, String>)  {
+
+    companion object {
+        const val AWS_ROLE_ARN = "AWS_ROLE_ARN"
+        const val AWS_WEB_IDENTITY_TOKEN_FILE = "AWS_WEB_IDENTITY_TOKEN_FILE"
+        const val AWS_S3_ACCESS_KEY_ID = "AWS_S3_ACCESS_KEY_ID"
+        const val AWS_S3_SECRET_ACCESS_KEY = "AWS_S3_SECRET_ACCESS_KEY"
+        const val AWS_S3_BUCKET = "AWS_S3_BUCKET"
+        const val AWS_S3_ENDPOINT = "AWS_S3_ENDPOINT"
+        const val AWS_S3_ACCELERATED = "AWS_S3_ACCELERATED"
+        const val NX_API_ROLE_SESSION = "nxApiRoleSession" // This is an arbitrary value
     }
-}
 
-fun getClient(env: Map<String, String>): AmazonS3 {
-    return if (env.containsKey("AWS_ROLE_ARN")) {
-        log.info("Role ARN provided, attempting to create client with temp credentials.")
 
-        val stsClient = AWSSecurityTokenServiceClientBuilder.standard()
-            .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-            .withRegion(Regions.US_EAST_1)
-            .build()
+    private var _client: AmazonS3? = null
+    private val client: AmazonS3
+        get() {
+            when {
+                _client == null -> {
+                    _client = getNewClient()
+                }
 
-        val token = File(env["AWS_WEB_IDENTITY_TOKEN_FILE"]).readText()
-        val roleRequest = AssumeRoleWithWebIdentityRequest()
-            .withRoleArn(env["AWS_ROLE_ARN"])
-            .withWebIdentityToken(token)
-            .withRoleSessionName("nxApiRoleSession")
+                env.containsKey(AWS_ROLE_ARN) && !isStillAuthenticated(_client) -> {
+                    _client?.shutdown()
+                    _client = getNewClient()
+                }
+            }
 
-        val roleResponse: AssumeRoleWithWebIdentityResult = stsClient.assumeRoleWithWebIdentity(roleRequest)
-        val sessionCredentials: Credentials = roleResponse.credentials
+            return _client ?: throw AssertionError("Amazon S3 Client unexpectedly null.")
+        }
 
-        AmazonS3ClientBuilder.standard()
-            .withCredentials(
-                AWSStaticCredentialsProvider(
-                    BasicSessionCredentials(
-                        sessionCredentials.accessKeyId,
-                        sessionCredentials.secretAccessKey,
-                        sessionCredentials.sessionToken
+    /**
+     * Unfortunately this is the best way I could find to check whether the credentials we have are
+     * still useful. We _could_ implement some kind of timer based on the expected timeout for
+     * the credentials we're given, but this is much simpler and possibly less error-prone
+     */
+    private fun isStillAuthenticated(s3: AmazonS3?): Boolean {
+        log.info("Checking whether we are still authenticated...")
+        if (s3 == null) {
+            log.error("Client is unexpectedly null, returning false.")
+            return false
+        }
+
+        return try {
+            s3.listObjects(ListObjectsRequest(BUCKET, null, null, null, 0))
+            true
+        } catch (ex: Exception) {
+            log.info("No longer authenticated with the current credentials.")
+            false
+        }
+    }
+
+    private fun getNewClient(): AmazonS3 {
+        log.info("Getting a new AmazonS3 client.")
+
+        val s3: AmazonS3 = if (env.containsKey(AWS_ROLE_ARN)) {
+            log.info("$AWS_ROLE_ARN provided, attempting to create client with temp credentials.")
+
+            val stsClient = AWSSecurityTokenServiceClientBuilder.standard()
+                .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
+                .build()
+
+            val token = env[AWS_WEB_IDENTITY_TOKEN_FILE]?.let { File(it).readText() }
+            val roleRequest = AssumeRoleWithWebIdentityRequest()
+                .withRoleArn(env[AWS_ROLE_ARN])
+                .withWebIdentityToken(token)
+                .withRoleSessionName(NX_API_ROLE_SESSION)
+
+            val roleResponse: AssumeRoleWithWebIdentityResult = stsClient.assumeRoleWithWebIdentity(roleRequest)
+            val sessionCredentials: Credentials = roleResponse.credentials
+
+            AmazonS3ClientBuilder.standard()
+                .withCredentials(
+                    AWSStaticCredentialsProvider(
+                        BasicSessionCredentials(
+                            sessionCredentials.accessKeyId,
+                            sessionCredentials.secretAccessKey,
+                            sessionCredentials.sessionToken
+                        )
                     )
                 )
-            )
-            .withRegion(Regions.US_EAST_1)
-            .build()
-    } else if (env.containsKey("AWS_ACCESS_KEY_ID") && env.containsKey("AWS_SECRET_ACCESS_KEY")) {
-        log.info("AWS_ACCESS_KEY_ID provided, creating client the old-fashioned way.")
-        AmazonS3ClientBuilder.standard()
-            .withCredentials(
-                AWSStaticCredentialsProvider(
-                    BasicAWSCredentials(env["AWS_ACCESS_KEY_ID"], env["AWS_SECRET_ACCESS_KEY"])
+                .build()
+        } else if (env.containsKey(AWS_S3_ACCESS_KEY_ID) && env.containsKey(AWS_S3_SECRET_ACCESS_KEY)) {
+            log.info("$AWS_S3_ACCESS_KEY_ID provided, creating client the old-fashioned way.")
+            AmazonS3ClientBuilder.standard()
+                .withCredentials(
+                    AWSStaticCredentialsProvider(
+                        BasicAWSCredentials(env[AWS_S3_ACCESS_KEY_ID], env[AWS_S3_SECRET_ACCESS_KEY])
+                    )
                 )
-            )
-            .withRegion(Regions.US_EAST_1)
-            .build()
-    } else {
-        val message = "No S3 credentials provided"
-        log.error(message)
-        throw Exception(message)
-    }
-}
+                .build()
+        } else {
+            throw Exception("No S3 credentials provided")
+        }
 
-fun cleanUp(s3: AmazonS3, key: String) {
-    log.info("Deleting object $BUCKET/$key...")
-    s3.deleteObject(BUCKET, key)
-    log.info("Object $BUCKET/$key deleted successfully!")
+        if (env.containsKey(AWS_S3_ENDPOINT)) {
+            s3.setEndpoint(env[AWS_S3_ENDPOINT])
+        }
+
+        if (env.containsKey(AWS_S3_ACCELERATED) && env[AWS_S3_ACCELERATED] == "TRUE") {
+            s3.setS3ClientOptions(S3ClientOptions.builder().setAccelerateModeEnabled(true).build())
+        }
+
+        return s3
+    }
+
+    fun putObject(key: String) {
+        log.info("Creating object $BUCKET/$key...")
+        client.putObject(BUCKET, key, "Testing with Kotlin SDK")
+        log.info("Object $BUCKET/$key created successfully!")
+    }
+
+
+    fun cleanUp(key: String) {
+        log.info("Deleting object $BUCKET/$key...")
+        client.deleteObject(BUCKET, key)
+        log.info("Object $BUCKET/$key deleted successfully!")
+    }
 }
